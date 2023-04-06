7835744c1a11752fdf6283ea50715e75619ca35c
alireza-mehrzad <alireza.mehrzad369@gmail.com>
2023-04-06 02:02:15 -0700

    episode page

--- script.js
+++ script.js
@@ -21,3 +21,89 @@ function setup(URL) {
       selectEpisode(json);
     });
 }
+
+function makeEpisodePage(list) {
+  const rootElem = document.getElementById("root");
+  list.forEach((episode) => {
+    let eachDiv = document.createElement(`div`);
+    let containerDiv = rootElem.appendChild(eachDiv);
+    containerDiv.setAttribute(`id`, `${episode.id}`);
+    containerDiv.setAttribute(`class`, `container episode`);
+    containerDiv.setAttribute(`style`, `display: initial;`);
+
+    //take the name of each episode and put it inside containerDiv
+    let nameParagraph = document.createElement(`p`);
+    containerDiv.appendChild(nameParagraph);
+    nameParagraph.setAttribute(
+      `id`,
+      `nameS${codeCorrection(episode.season)}E${codeCorrection(episode.number)}`
+    );
+    nameParagraph.setAttribute(`class`, `name`);
+    nameParagraph.textContent = `${episode.name}`;
+
+    //take the episode code
+    let episodeCodeP = document.createElement(`p`);
+    containerDiv.appendChild(episodeCodeP);
+    episodeCodeP.setAttribute(
+      `id`,
+      `episodeCodeS${codeCorrection(episode.season)}E${codeCorrection(
+        episode.number
+      )}`
+    );
+    episodeCodeP.setAttribute(`class`, `episodeCode`);
+    episodeCodeP.textContent = `Episode Code: S${codeCorrection(
+      episode.season
+    )}E${codeCorrection(episode.number)}`;
+
+    //take the episode Images
+    let episodeImg = document.createElement(`img`);
+    let divImg = document.createElement(`div`);
+    divImg.appendChild(episodeImg);
+    containerDiv.appendChild(divImg);
+    divImg.setAttribute(`class`, `divImg`);
+    episodeImg.setAttribute(
+      `id`,
+      `episodeImgS${codeCorrection(episode.season)}E${codeCorrection(
+        episode.number
+      )}`
+    );
+    episodeImg.setAttribute(`class`, `episodeImg`);
+    episodeImg.src = episode.image.medium;
+    episodeImg.alt = `image`;
+
+    //take the summary of each episode
+    let episodeSummary = document.createElement(`article`);
+    let summaryArticle = containerDiv.appendChild(episodeSummary);
+    episodeSummary.setAttribute(
+      `id`,
+      `summaryS${codeCorrection(episode.season)}E${codeCorrection(
+        episode.number
+      )}`
+    );
+    episodeSummary.setAttribute(`class`, `summary`);
+
+    //summary tag inside article
+    let summaryTagP = document.createElement(`p`);
+    summaryArticle.appendChild(summaryTagP);
+    summaryTagP.setAttribute(
+      `id`,
+      `summaryTagS${codeCorrection(episode.season)}E${codeCorrection(
+        episode.number
+      )}`
+    );
+    summaryTagP.setAttribute(`class`, `summaryTag`);
+    summaryTagP.textContent = `Summary`;
+
+    //actual summary inside article
+    let summary = document.createElement(`p`);
+    summaryArticle.appendChild(summary);
+    summary.setAttribute(
+      `id`,
+      `summaryPS${codeCorrection(episode.season)}E${codeCorrection(
+        episode.number
+      )}`
+    );
+    summary.setAttribute(`class`, `summaryP`);
+    summary.textContent = episode.summary.replace(/(<([^>]+)>)/gi, "");
+  });
+}
